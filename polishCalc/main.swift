477909044e0eace398839ffbd12f672fadd56b83
Nikita Padalko <m1dn1ght3@icloud.com>
2022-07-07 21:25:08 +0900

    This is the first iteration of my Calculator in Polish Notation
    Realised Stack programmaticaly
    Alghoritm of calculation

--- polishCalc/main.swift
+++ polishCalc/main.swift
@@ -1,11 +1,60 @@
-//
-//  main.swift
-//  polishCalc
-//
-//  Created by Никита Падалко on 22.04.2022.
-//
 
 import Foundation
+struct Stack<Element> {
+    var stack = [Element]()
+    
+    mutating func push(_ item : Element) {
+        stack.append(item)
+    }
+    
+    mutating func pop() -> Element? {
+        return stack.popLast()
+    }
+    func peek() -> Element? {
+        return stack.last
+    }
+}
 
-print("Hello, World!")
+var numbersStack = Stack<Int>()
+var symbolsStack = Stack<String>()
+
+print("Введите пример для вычисления в Польской Нотации")
+var userInput = readLine()
+var separatedInput = [String]()
+
+if let input = userInput {
+    var temp = ""
+    for symbol in input {
+        if symbol != " " {
+            temp += String(symbol)
+        }
+        if symbol == " " {
+            separatedInput.append(temp)
+            temp = ""
+        }
+    }
+    separatedInput.append(temp)
+}
+print(separatedInput)
+func calculate(_ : String?) {
+    for symbol in separatedInput.reversed() {
+        // print(symbol)
+        switch symbol {
+        case "+", "-", "*", "/" : symbolsStack.push(symbol)
+            switch symbolsStack.pop()! {
+            case "+" : numbersStack.push(numbersStack.pop()! + numbersStack.pop()!)
+            case "-" : numbersStack.push(numbersStack.pop()! - numbersStack.pop()!)
+            case "*" : numbersStack.push(numbersStack.pop()! * numbersStack.pop()!)
+            case "/" : numbersStack.push(numbersStack.pop()! / numbersStack.pop()!)
+            default : break
+            }
+        default : numbersStack.push(Int(symbol)!)
+            }
+    }
+    print("Результат вычислений : \(numbersStack.pop()!)")
+}
+
+calculate(userInput)
+
+
 
